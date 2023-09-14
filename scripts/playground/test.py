aebe6e3d74efc0334057cdcc15cdebd4a6e0b234
Thrud Primrose <budanaz.yakup@gmail.com>
2023-09-14 08:59:46 +0200

    Update refs to submodules

--- scripts/playground/test.py
+++ scripts/playground/test.py
@@ -9,14 +9,14 @@ def add_trace(g):
     kernel = c <= A["ii"]
     g.add(name='kernel', ast=kernel, target="c")
 
-def add_simple_tensor(g):
+def add_simple_tensor(g, target):
     N = 8
     A = Tensor('A', (N, 2*N, N))
     B = Tensor('B', (N, 2*N, N))
     C = Tensor('C', (N, 2*N, N))
 
-    kernel = C['ijl'] <= A['ijk'] * B['kjl']
-    g.add(name='kernel', ast=kernel, target="gpu")
+    kernel = C['zxa'] <= A['zxy'] * B['yxa']
+    g.add(name='kernel', ast=kernel, target=target)
 
 def add_weird_tensor(g):
     N = 8
@@ -35,13 +35,13 @@ def add_complex_matrix(g):
     kernel = C <= A['ik'] * B['ki']
     g.add(name='kernel', ast=kernel, target="gpu")
 
-def add_matrix(g):
-    N = 8
+def add_matrix(g, target):
+    N = 9
     A = Tensor("A", (N, N))
     B = Tensor("B", (N, N))
     C = Tensor("C", (N, N))
     kernel = C['ij'] <= A['ik'] * B['kj']
-    g.add(name='kernel', ast=kernel, target="gpu")
+    g.add(name='kernel', ast=kernel, target=target)
 
 def add_example_tensor(g):
     N = 8
@@ -57,12 +57,14 @@ def add_example_tensor(g):
 arch = useArchitectureIdentifiedBy(
     host_arch="shsw", device_arch="ssm_86", device_backend="cuda")
 generator = Generator(arch)
-#add_simple_tensor(generator)
+add_simple_tensor(generator, "cpu")
+add_simple_tensor(generator, "gpu")
 #add_complex_matrix(generator)
-add_matrix(generator)
+add_matrix(generator, "cpu")
+add_matrix(generator, "gpu")
 #add_weird_tensor(generator)
 #add_trace(generator)
 
 directory = os.path.dirname(os.path.abspath(__file__))
 generator.generate(outputDir=directory,
-                   gemm_cfg=GeneratorCollection([GemmForge(arch)]))
+                   gemm_cfg=GeneratorCollection([GemmForge(arch), Eigen(arch)]))
