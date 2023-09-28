1a3714171560f6272f2d0b880b47fa216cf42175
thrudprimrose <budanaz.yakup@gmail.com>
2023-09-28 12:24:15 +0200

    Rm stale files

--- scripts/play.py
+++
@@ -1,9 +0,0 @@
-grouped_list = [list(range(i, i + 32)) for i in range(0, 1024, 32)]
-
-# Printing the result to confirm
-s = "{\n"
-for group in grouped_list:
-    s += "{" + ", ".join([str(x) for x in group]) + "},\n"
-s += "}\n"
-
-print(s)
