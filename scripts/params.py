6d967bdb51dec7e0ef8880b34d0833970e6fe292
thrudprimrose <budanaz.yakup@gmail.com>
2023-10-25 17:50:33 +0200

    Update default params

--- scripts/params.py
+++ scripts/params.py
@@ -53,7 +53,9 @@ my_blue = "#6ab2ca"
 sparse_rose = "#c74375"
 dense_blue = my_blue
 #pre = "TensorKernel1-NewLoopHeuristics"
-pre = "Product-N18"
+#pre = "TensorKernel2-NoLoopHeuristics"
+#pre = "TensorKernel3-NewLoadStrategy"
+pre = "TensorKernel2-LoadBoth"
 
 # Change always
 workaround = False
@@ -80,11 +82,13 @@ mat_lib_lib = "/opt/nvidia/hpc_sdk/Linux_x86_64/23.7/math_libs/11.8/lib64"
 
 #flow_generator_list = [f"{scripts_dir}/benchmark_dense_sparse_cuda.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_sparse_dense_cuda.py"]
-flow_generator_list = [f"{scripts_dir}/benchmark_product.py"]
+#flow_generator_list = [f"{scripts_dir}/benchmark_product.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_tensor1.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_product_2.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_loop.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_tensor_unfuse.py"]
-#flow_generator_list = [f"{scripts_dir}/benchmark_tensor2.py"]
+flow_generator_list = [f"{scripts_dir}/benchmark_tensor2.py"]
 #flow_generator_list = [f"{scripts_dir}/benchmark_tensor3.py"]
 write_output = True
+
+sort = True
